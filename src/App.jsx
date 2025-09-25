27b7b9678483366bdb47356deaf55e060b8f8d73
ebuka nathan <ebukanathan@gmail.com>
2025-09-25 23:57:04 +0100

    cordinate

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./components/Nav";
 import Search from "./components/Search";
 import MainBody from "./components/MainBody";
 
 function App() {
+  const [location, setLocation] = useState({ lat: null, lon: null });
+  const [error, setError] = useState("");
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setError("not supported");
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLocation({
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+      });
+      setError(null);
+    });
+  };
+
+  //get brower coordinate
+
   return (
     <div className="w-2/3 mx-auto ">
       <Nav />
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Search() {
+  const [city, setCity] = useState("");
+  const [search, setSearch] = useState("");
+
+  const HandleSubmit = (e) => {
+    e.prevent.default();
+    setSearch(city);
+    // alert(`${search} is submitted`);
+    console.log(`${search} is submitted`);
+  };
+
   return (
-    <div className="mt-6 w-1/2 mx-auto flex gap-2">
-      <input
-        type="text"
-        className="p-2 outline-1 outline-gray-300 rounded-md w-full"
-        placeholder="Search for any city"
-      />
-      <button className="bg-blue-600 text-white p-2 rounded-md">Search</button>
+    <div className="">
+      <form className="mt-6 w-1/2 mx-auto flex gap-2" onSubmit={HandleSubmit}>
+        <input
+          type="text"
+          value={city}
+          className="p-2 outline-1 outline-gray-300 rounded-md w-full"
+          placeholder="Search for any city"
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button className="bg-blue-600 text-white p-2 rounded-md">
+          Search
+        </button>
+      </form>
     </div>
   );
 }
