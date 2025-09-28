80051f24450444356c4042485622e73b9bd595c4
ebuka nathan <ebukanathan@gmail.com>
2025-09-28 22:55:04 +0100

    cordinate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Nav from "./components/Nav";
 import Search from "./components/Search";
 import MainBody from "./components/MainBody";
+import { fetchWeatherApi } from "openmeteo";
 
 function App() {
   const [location, setLocation] = useState({ lat: null, lon: null });
@@ -13,16 +14,31 @@ function App() {
       return;
     }
 
-    navigator.geolocation.getCurrentPosition((position) => {
-      setLocation({
-        lat: position.coords.latitude,
-        lon: position.coords.longitude,
-      });
-      setError(null);
-    });
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLocation({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        });
+        setError(null);
+      },
+      (error) => {
+        setError("error getting location" + error.message);
+      }
+    );
   };
 
-  //get brower coordinate
+  const params = {
+    longitude: location.lon,
+    latitude: location.lat,
+    hourly: "temperature_2m",
+  };
+  const url = "https://api.open-meteo.com/v1/forecast";
+
+  const responses = fetchWeatherApi(url, params);
+  const response = responses[0];
+
+  console.log(response);
 
   return (
     <div className="w-2/3 mx-auto ">
@@ -30,6 +46,7 @@ function App() {
       <div className="w-2/3 mx-auto mt-6  text-center font-bold text-3xl">
         How’s the sky looking today?
       </div>
+      <button onClick={getLocation}>get location now</button>
       <Search />
       <MainBody />
     </div>
