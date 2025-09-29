4601a9a521331a3e7b2cef9d356b9cef0cdec866
ebuka nathan <ebukanathan@gmail.com>
2025-09-29 19:07:55 +0100

    search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Search() {
   const [city, setCity] = useState("");
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState([]);
 
   const HandleSubmit = (e) => {
-    e.prevent.default();
-    setSearch(city);
-    // alert(`${search} is submitted`);
-    console.log(`${search} is submitted`);
+    e.preventDefault();
+
+    console.log(`${city} is submitted`);
+    getSearchLocation(city);
   };
 
+  const getSearchLocation = async (location) => {
+    try {
+      const response = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${location}&count=10&language=en&format=json`
+      );
+
+      const data = await response.json();
+      setSearch(data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log(search);
+
   return (
-    <div className="">
+    <div className="relative">
       <form className="mt-6 w-1/2 mx-auto flex gap-2" onSubmit={HandleSubmit}>
         <input
           type="text"
@@ -25,6 +40,20 @@ function Search() {
           Search
         </button>
       </form>
+      <div className=" ">
+        {search.length > 0 ? (
+          <div className="w-1/3  h-[30vh] mb-1 absolute top-[100%] left-[25%] bg-amber-500 text-black overflow-scroll flex flex-col">
+            {search?.map((item, index) => (
+              <div key={index} className="hover:bg-amber-50 cursor-pointer">
+                {" "}
+                {item.name},{item.country}
+              </div>
+            ))}
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 }
